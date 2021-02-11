48b71ec1472ecf8f5cc4a695452a49c8163b9c2f
Pavel Monier <mofender1@gmail.com>
2021-02-12 00:27:52 +0100

    Even testen

--- app/src/main/java/nl/hva/optimuz/MainActivity.kt
+++ app/src/main/java/nl/hva/optimuz/MainActivity.kt
@@ -1,6 +1,8 @@
 package nl.hva.optimuz
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -22,5 +24,15 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        // Wijzing naar button
+        val testBtn = findViewById(R.id.testBtn) as Button
+        // hier on click
+        testBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Optimuz says:'Hello world!'", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
