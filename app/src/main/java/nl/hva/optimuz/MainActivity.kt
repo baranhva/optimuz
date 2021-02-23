b853b681c46d4f6f81ddef71399a01ac728e3fc2
Corniels de Best <cornielsdebest@hotmail.com>
2021-02-23 13:47:37 +0100

    login draft

--- app/src/main/java/nl/hva/optimuz/MainActivity.kt
+++ app/src/main/java/nl/hva/optimuz/MainActivity.kt
@@ -1,10 +1,11 @@
 package nl.hva.optimuz
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,16 +16,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_settings))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_settings))
+        val loginButton = findViewById<Button>(R.id.login_button)
+        val loginView = findViewById<TableLayout>(R.id.loginView)
+        val mainView = findViewById<FrameLayout>(R.id.mainView)
+        val feedback = findViewById<TextView>(R.id.feedback)
 
+        loginButton.setOnClickListener {
+            val loginEmail = findViewById<EditText>(R.id.login_email).text.toString()
+            val loginPassword = findViewById<EditText>(R.id.login_password).text.toString()
+
+            if (loginEmail == "email" && loginPassword == "pass"){
+                setupActionBarWithNavController(navController, appBarConfiguration)
+                navView.setupWithNavController(navController)
+                loginView.isVisible = false
+                mainView.isVisible = true
+                navView.isVisible = true
+            }else{
+                feedback.text = "Invalid email/password combination"
+            }
+
+        }
 
     }
+
 }
