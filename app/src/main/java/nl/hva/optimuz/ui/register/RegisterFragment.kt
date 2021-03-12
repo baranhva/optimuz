62886554d5c6ff9748eb5c97e48acc5e17e09ebb
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-12 12:08:07 +0100

    move code from register_fragment to fragment_register and rename attribute names. Also only use hint text. renamed attribute names in strings.xml and use same attributes for email and password in login and register

--- app/src/main/java/nl/hva/optimuz/ui/register/RegisterFragment.kt
+++ app/src/main/java/nl/hva/optimuz/ui/register/RegisterFragment.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import nl.hva.optimuz.MainActivity
 import androidx.lifecycle.ViewModelProvider
 import nl.hva.optimuz.R
 
@@ -24,7 +20,7 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.register_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
